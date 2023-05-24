357ab25ae35fba27f1715701e7aee921ca36b7d8
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-24 17:03:30 +0200

    add page controller

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers\Guest;
 
+use App\Models\Movie;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function movie()
+    {
+
+        $movies = Movie::all();
+        // dd($movies);
+        return view('movies', compact('movies'));
+    }
 }
