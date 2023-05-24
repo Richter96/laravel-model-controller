ab89c4d0033cca35c21ffa4d02e5b6211f4019f7
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-24 17:03:56 +0200

    add structure films

---
+++ resources/views/movies.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <h1>Same Good Films</h1>
+
+    <div class="container">
+        <div class="row row-cols-xl-3">
+            @forelse ($movies as $movie)
+                <div class="col">
+                    <div class="card text-start">
+                        <img class="card-img-top" src="holder.js/100px180/" alt="Title">
+                        <div class="card-body">
+                            <h3 class="card-title">{{ $movie->title }}</h3>
+                            <h5 class="card-title">{{ $movie->original_title }}</h5>
+                            <span class="card-text">Voto: {{ $movie->vote }}</span>
+                        </div>
+                    </div>
+                </div>
+            @empty
+                <p>Qui non ci sono Film a disposizione </p>
+            @endforelse
+        </div>
+    </div>
+@endsection
