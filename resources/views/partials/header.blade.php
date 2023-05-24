c2432e0f75ee7e507a9d408fc86f28627c1c2bbc
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-05-24 17:04:34 +0200

    add header page

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,6 @@
+<header>
+    <nav class="nav nav-tabs">
+        <a class="nav-link active" href="{{ route('home') }}" aria-current="page">Home</a>
+        <a class="nav-link" href="{{ route('movies') }}">Movies</a>
+    </nav>
+</header>
